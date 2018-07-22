f58c037ebafa85fc12a9b6b479f109288faddd24
Jon Nordby <jononor@gmail.com>
2018-07-22 11:37:17 +0200

    Tool for creating file lists for test sets

---
+++ filelist.py
@@ -0,0 +1,20 @@
+
+import os
+import sys
+
+import pandas
+import numpy
+
+def main():
+    path, out, dataset = sys.argv[1], sys.argv[2], sys.argv[3]
+    files = os.listdir(path)    
+
+    df = pandas.DataFrame({
+        'itemid': files,
+        'datasetid': [dataset for f in files],
+        'hasbird': [None for f in files],
+    })
+    df.to_csv(out, index=False)
+
+if __name__ == '__main__':
+    main()
