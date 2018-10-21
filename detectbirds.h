ade730793fdd7f07b5c37c3e10c6190d2df69f59
Jon Nordby <jononor@gmail.com>
2018-10-21 02:13:16 +0200

    Move audio code to emlearn
    https://github.com/jonnor/emlearn

--- detectbirds.h
+++ detectbirds.h
@@ -11,61 +11,61 @@ extern "C" {
 #include <stdbool.h>
 #include <stdlib.h>
 
-#include "emfft.h"
-#include "emaudio.h"
+#include <eml_fft.h>
+#include <eml_audio.h>
 
 
-// FIXME: actually include emtrees
-//#include <emtrees.h>
+// FIXME: actually include eml_trees
+//#include <eml_trees.h>
 typedef struct {} EmtreesModel;
 int32_t
-emtrees_predict(EmtreesModel *m, float *f, int32_t l) {
+eml_trees_predict(EmlTreesModel *m, float *f, int32_t l) {
     return 0;
 }
-EmtreesModel *birddetect_model = NULL;
+EmlTreesModel *birddetect_model = NULL;
 
 
 
 // birddetector.h
 typedef struct _BirdDetector {
-    EmVector audio;
-    EmVector features;
-    EmVector temp1;
-    EmVector temp2;
-    EmAudioMel mel_filter;
-    EmtreesModel *model;
+    EmlVector audio;
+    EmlVector features;
+    EmlVector temp1;
+    EmlVector temp2;
+    EmlAudioMel mel_filter;
+    EmlTreesModel *model;
 } BirdDetector;
 
 
 void
 birddetector_reset(BirdDetector *self) {
 
-    emvector_set_value(self->audio, 0.0f);
-    emvector_set_value(self->features, 0.0f);
-    emvector_set_value(self->temp1, 0.0f);
-    emvector_set_value(self->temp2, 0.0f);
+    eml_vector_set_value(self->audio, 0.0f);
+    eml_vector_set_value(self->features, 0.0f);
+    eml_vector_set_value(self->temp1, 0.0f);
+    eml_vector_set_value(self->temp2, 0.0f);
 }
 
 void
 birddetector_push_frame(BirdDetector *self, EmVector frame) {
 
     // insert new frame into our buffer
-    emvector_shift(self->audio, -frame.length);
-    emvector_set(self->audio, frame, self->audio.length-frame.length);
+    eml_vector_shift(self->audio, -frame.length);
+    eml_vector_set(self->audio, frame, self->audio.length-frame.length);
 
     // process current window
-    emvector_set(self->temp1, self->audio, 0);
-    emaudio_melspectrogram(self->mel_filter, self->temp1, self->temp2);
-    emvector_meansub(self->temp1);
+    eml_vector_set(self->temp1, self->audio, 0);
+    eml_audio_melspectrogram(self->mel_filter, self->temp1, self->temp2);
+    eml_vector_meansub(self->temp1);
 
     // Feature summarization
-    emvector_max_into(self->features, emvector_view(self->temp1, 0, self->features.length));
+    eml_vector_max_into(self->features, emvector_view(self->temp1, 0, self->features.length));
 }
 
 bool
 birddetector_predict(BirdDetector *self) {
 
-    const int32_t cl = emtrees_predict(self->model, self->features.data, self->features.length);
+    const int32_t cl = eml_trees_predict(self->model, self->features.data, self->features.length);
     return cl == 1;
 }
 
